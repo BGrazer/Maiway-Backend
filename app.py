444eeaa9713537313872225f14ca830fa9edc8c7
BGrazer <bravenreyes@gmail.com>
2025-07-16 07:08:38 +0800

    revert

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_cors import CORS
 # Import Blueprints
 from rfr import rfr_bp
 from chatbot import chatbot_bp
-from routing import routing_bp
 
 # Initialize Flask App
 app = Flask(__name__)
@@ -13,7 +12,6 @@ CORS(app)
 # Register Blueprints
 app.register_blueprint(rfr_bp)
 app.register_blueprint(chatbot_bp)
-app.register_blueprint(routing_bp)
 
 if __name__ == '__main__':
     print(f"\n🚀 Combined backend running at: http://0.0.0.0:5001\n")
